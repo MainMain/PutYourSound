941762df4b8b078881a3e2fda301bae3ae30dd41
root <root@gladys.lan>
2015-02-06 16:37:46 +0100

    Inclusion Mustache + Page de demo

--- index.js
+++ index.js
@@ -1,16 +1,41 @@
+// API express
 var express = require('express')
-var app = express()
+var app = express();
+// API FileSystem
+var fs = require('fs'); 
+// IMPORT mustach Engine
+var mustache = require('mustache'); 
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+// Données de test
+var demo = [{ 
+            "projet": "PutYourSound",
+            "team": [{
+                "member": "Johan"
+            }, {
+                "member": "Joris"
+            }, {
+                "member": "Julien"
+            }, {
+                "member": "Roy"
+            }]}];
 
-var server = app.listen(3000, function () {
+// Route principale
+app.get('/:slug', function(req, res){ // get the url and slug info
+var slug =[req.params.slug][0]; // grab the page slug
 
-  var host = server.address().address
-  var port = server.address().port
+// Données à renvoyer (a modifier pour les websockets ?)
+var rData = {data:demo}; // wrap the data in a global object... (mustache starts from an object then parses)
 
-  console.log('Example app listening at http://%s:%s', host, port)
+// Création de la page
+var page = fs.readFileSync(slug, "utf8"); // bring in the HTML file
 
-})
+// Conversion des données avec mustache
+var html = mustache.to_html(page, rData); // replace all of the data
 
+// Envoi de la page convertie au client
+res.send(html); // send to client
+});
+
+// Lancement du serveur
+app.listen(3000);// start the server listening
+console.log('Server running at http://127.0.0.1:3000/'); // server start up message
